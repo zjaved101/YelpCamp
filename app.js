7c317fae6fa874a42b3aa475a6d79c53e0e5ce62
Zan Javed <zjaved101@users.noreply.github.com>
2018-10-14 00:38:26 -0700

    Layout (#1)
    * Added header and footer partials
    
    * Added bootstrap
    
    * added body-parser
    
    * Added ability to add new campgrounds
    
    * added image grid and layout responsiveness
    
    * added styling
    
    * cleaned up routes
    
    * added travis ci
    
    * updated travis ci
    
    * Delete .travis.yml
    
    * update for testing
    
    * updated scripts

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const port = 3000;
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res) {
+let campgrounds = [
+    {name: 'Salmon Creek', image: 'https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104496f8c97aa4e5bdb9_340.jpg'},
+    {name: 'Granite Hill', image: 'https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104496f8c97aa4e5bdb9_340.jpg'},
+    {name: "Mountain Goat's Rest", image: 'https://pixabay.com/get/e834b5062cf4033ed1584d05fb1d4e97e07ee3d21cac104496f8c97aa4e5bdb9_340.jpg'},
+]
+
+app.get('/', (req, res) => {
     res.render('landing');
 });
 
-app.get('/campgrounds', function(req, res) {
-    let campgrounds = [
-        {name: 'Salmon Creek', image: 'https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104496f8c97aa4e5bdb9_340.jpg'},
-        {name: 'Granite Hill', image: 'https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104496f8c97aa4e5bdb9_340.jpg'},
-        {name: "Mountain Goat's Rest", image: 'https://pixabay.com/get/e834b5062cf4033ed1584d05fb1d4e97e07ee3d21cac104496f8c97aa4e5bdb9_340.jpg'},
-    ]
-
+app.get('/campgrounds', (req, res) => {
     res.render('campgrounds', {campgrounds: campgrounds});
+});
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('new.ejs');
+});
+
+app.post('/campgrounds', (req, res) => {
+    let name = req.body.name;
+    let image = req.body.image;
+    campgrounds.push({name: name, image: image});
+    res.redirect('/campgrounds');
 });
 
 app.listen(port, () => {
